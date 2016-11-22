64376c324e8f27966941b92fff217f850a01c961
oh geun taek <rmsxor94@naver.com>
2016-11-22 21:13:33 +0900

    modefied dbManager

--- LocationDiary/app/src/main/java/com/taek_aaa/locationdiary/DBManager.java
+++ LocationDiary/app/src/main/java/com/taek_aaa/locationdiary/DBManager.java
@@ -9,32 +9,31 @@ import android.util.Log;
 public class DBManager extends SQLiteOpenHelper {
     public static double curlatitude;
     public static double curlongitude;
-    public static Cursor c;
 
     public DBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, name, null, 1);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
         // 새로운 Table 생성
-        db.execSQL("CREATE TABLE database (_id INTEGER PRIMARY KEY AUTOINCREMENT, latitude DOUBLE , longitude DOUBLE);");
+        db.execSQL("CREATE TABLE gpstable (id INTEGER PRIMARY KEY AUTOINCREMENT, latitude double , longitude double);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists gpstable;");
     }
 
     public void insert(double latitude, double longitude) {
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("INSERT INTO database VALUES(NULL, " + latitude + ", " + longitude + ");");
-        String str = "Latitude: " + latitude + "\n" + "Longitude: " + longitude + "\n";
+        db.execSQL("INSERT INTO gpstable VALUES(NULL, " + latitude + ", " + longitude + ");");  //string넣을때는 '' 하고그안에""해야
         db.close();
     }
 
     public void getResult() {
         SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM database", null);
-        c = cursor;
+        Cursor cursor = db.rawQuery("SELECT * FROM gpstable", null);
+       // c = cursor;
         while (cursor.moveToNext()) {
             double latitudecur = cursor.getDouble(cursor.getColumnIndex("latitude"));
             double longitudecur = cursor.getDouble(cursor.getColumnIndex("longitude"));
@@ -42,5 +41,7 @@ public class DBManager extends SQLiteOpenHelper {
             curlatitude = latitudecur;
             curlongitude = longitudecur;
         }
+        cursor.close();
+        db.close();
     }
 }
