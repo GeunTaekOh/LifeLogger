fd78d360d75e13aa43dd9f776f0926821d977a7c
oh geun taek <rmsxor94@naver.com>
2016-11-29 01:20:30 +0900

    ui really finished!

--- MP_TermProject/app/src/main/java/com/taek_aaa/locationdiary/GoalActivity.java
+++ MP_TermProject/app/src/main/java/com/taek_aaa/locationdiary/GoalActivity.java
@@ -1,7 +1,9 @@
 package com.taek_aaa.locationdiary;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by taek_aaa on 2016. 11. 27..
@@ -13,4 +15,9 @@ public class GoalActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal);
     }
+
+    public void onClickSetGoal(View v){
+        startActivity(new Intent(this, GoalSettingActivity.class));
+    }
+
 }
---
+++ MP_TermProject/app/src/main/java/com/taek_aaa/locationdiary/GoalSettingActivity.java
@@ -0,0 +1,17 @@
+package com.taek_aaa.locationdiary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by taek_aaa on 2016. 11. 29..
+ */
+
+public class GoalSettingActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goalsetting);
+    }
+
+}
